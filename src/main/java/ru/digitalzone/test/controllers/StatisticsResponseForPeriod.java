20da948be672f1bde166b9d24e71cec033a22487
Valentin Skeeba <valentin.skeeba@citeck.ru>
2018-07-11 13:05:48 +0300

    use more lombok

--- src/main/java/ru/digitalzone/test/controllers/StatisticsResponseForPeriod.java
+++ src/main/java/ru/digitalzone/test/controllers/StatisticsResponseForPeriod.java
@@ -1,9 +1,11 @@
 package ru.digitalzone.test.controllers;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class StatisticsResponseForPeriod extends StatisticsResponseFor24Hours {
     private Long regularUsersForPeriod;
 
--- src/main/java/ru/digitalzone/test/controllers/VisitEventsController.java
+++ src/main/java/ru/digitalzone/test/controllers/VisitEventsController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.digitalzone.test.entities.VisitEvent;
 import ru.digitalzone.test.services.EventService;
 
@@ -12,7 +15,6 @@ import java.time.LocalDate;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
-@RequestMapping
 public class VisitEventsController {
 
     private final EventService eventService;
